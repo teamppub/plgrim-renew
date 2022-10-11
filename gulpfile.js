618795b04004456739e606791731c637e14cdfbf
김민혜 <kmhkgh@naver.com>
2022-10-11 14:38:34 +0900

    git hosting check

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@ var gulp = require('gulp');
 var pump = require('pump');
 var phpinc = require('php-include-html');
 var replace = require('gulp-replace');
-var setFilesGuide = ['list/*.html'];
+var setFilesGuidelist = ['list/*.html'];
+var setFilesGuide = ['pc/guide/*.html'];
 var setFilesPC = ['pc/html/**/*.html'];
 
+gulp.task('list', function (cb) {
+  pump(
+    [
+      gulp.src(setFilesGuidelist),
+      replace('../', ' '),
+      replace('./', '' + __dirname + '/list/'),
+      phpinc({ verbose: true }),
+      gulp.dest('build/html/list'),
+    ],
+    cb
+  );
+});
 gulp.task('pchtml', function (cb) {
   pump(
     [
@@ -12,20 +25,24 @@ gulp.task('pchtml', function (cb) {
       replace('../layout/', '' + __dirname + '/pc/html/layout/'),
       phpinc({ verbose: true }),
       gulp.dest('build/html/pc'),
-      gulp.src('pc/css/*.css'),
-      gulp.dest('build/css'),
     ],
     cb
   );
 });
-gulp.task('guide', function (cb) {
+gulp.task('pcguide', function (cb) {
   pump(
     [
       gulp.src(setFilesGuide),
-      replace('./', '' + __dirname + '/pc/html/layout/'),
+      replace('./', '' + __dirname + '/list/'),
       phpinc({ verbose: true }),
-      gulp.dest('build/html/list'),
+      gulp.dest('build/html/pc/guide'),
     ],
     cb
   );
+});
+gulp.task('pccss', function (cb) {
+  pump([gulp.src('pc/css/*.css'), gulp.dest('build/css')], cb);
+});
+gulp.task('pcguidecss', function (cb) {
+  pump([gulp.src('pc/guide/*.css'), gulp.dest('build/html/pc/guide')], cb);
 });
