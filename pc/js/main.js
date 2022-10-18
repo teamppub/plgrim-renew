accc09de7d4af5b10e07ed6aba4134e6712bce39
김민혜 <kmhkgh@naver.com>
2022-10-18 14:00:45 +0900

    Fix: work list 해상도 작을 경우 이미지 사이즈 조정

--- pc/js/main.js
+++ pc/js/main.js
@@ -1,54 +1,61 @@
 $(document).ready(function () {
-	//product
-	var prdPos = [];
-	var visualSwiper = new Swiper(".visual-swiper", {
-		loop: true,
-        loopAdditionalSlides: 100,
-        slidesPerView: 1.2,
-        spaceBetween: 100,
-        centeredSlides: true,
-        pagination: {
-            el: '.swiper-paging',
-            type: 'fraction',
-            formatFractionCurrent: function (number) {
-                return ('0' + number).slice(-2);
-            },
-            formatFractionTotal: function (number) {
-                return ('0' + number).slice(-2);
-            },
-            renderFraction: function (currentClass, totalClass) {
-                return '<em class="' + currentClass + '"></em>' +
-                    '<span> / </span>' +
-                    '<em class="' + totalClass + '"></em>';
-            }
-        },
-        navigation: {
-            nextEl: ".swiper-button-box .button-next",
-            prevEl: ".swiper-button-box .button-prev",
-        },
-	});
-            
-    if (matchMedia("screen and (min-width: 1640px)").matches) { //1640보다 크면
-        window.onload = () => {
-            document.querySelectorAll(".work-item").forEach((item) => {
-                item.style.gridRowEnd = `span ${item.clientHeight + 140}`;
-            });
-            const wrap = document.querySelector(".work-item-list");
-            wrap.style.display = "grid";
-            wrap.style.gridTemplateColumns = "repeat(2, 670px)";
-            wrap.style.gap = "0px 60px";
-            wrap.style.gridAutoRows = "1px";
-        }
-    } else {
-        window.onload = () => {
-            document.querySelectorAll(".work-item").forEach((item) => {
-                item.style.gridRowEnd = `span ${item.clientHeight + 70}`;
-            });
-            const wrap = document.querySelector(".work-item-list");
-            wrap.style.display = "grid";
-            wrap.style.gridTemplateColumns = "repeat(2, 600px)";
-            wrap.style.gap = "0px 60px";
-            wrap.style.gridAutoRows = "1px";
-        }
-    }
+  //product
+  var prdPos = [];
+  var visualSwiper = new Swiper('.visual-swiper', {
+    loop: true,
+    loopAdditionalSlides: 100,
+    slidesPerView: 1.2,
+    spaceBetween: 100,
+    centeredSlides: true,
+    pagination: {
+      el: '.swiper-paging',
+      type: 'fraction',
+      formatFractionCurrent: function (number) {
+        return ('0' + number).slice(-2);
+      },
+      formatFractionTotal: function (number) {
+        return ('0' + number).slice(-2);
+      },
+      renderFraction: function (currentClass, totalClass) {
+        return (
+          '<em class="' +
+          currentClass +
+          '"></em>' +
+          '<span> / </span>' +
+          '<em class="' +
+          totalClass +
+          '"></em>'
+        );
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-box .button-next',
+      prevEl: '.swiper-button-box .button-prev',
+    },
+  });
+
+  if (matchMedia('screen and (min-width: 1640px)').matches) {
+    //1640보다 크면
+    window.onload = () => {
+      document.querySelectorAll('.work-item').forEach(item => {
+        item.style.gridRowEnd = `span ${item.clientHeight + 140}`;
+      });
+      const wrap = document.querySelector('.work-item-list');
+      wrap.style.display = 'grid';
+      wrap.style.gridTemplateColumns = 'repeat(2, 670px)';
+      wrap.style.gap = '0px 60px';
+      wrap.style.gridAutoRows = '1px';
+    };
+  } else {
+    window.onload = () => {
+      document.querySelectorAll('.work-item').forEach(item => {
+        item.style.gridRowEnd = `span ${item.clientHeight + 70}`;
+      });
+      const wrap = document.querySelector('.work-item-list');
+      wrap.style.display = 'grid';
+      wrap.style.gridTemplateColumns = 'repeat(2, 570px)';
+      wrap.style.gap = '0px 60px';
+      wrap.style.gridAutoRows = '1px';
+    };
+  }
 });
