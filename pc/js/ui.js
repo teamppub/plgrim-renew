5fffe68cc399e65b2293ebff1e3d6b5a61e24823
이주연 aspyn <dldbsrud0925@gmail.com>
2022-10-18 10:43:08 +0900

    recruit

--- pc/js/ui.js
+++ pc/js/ui.js
@@ -15,32 +15,35 @@ var UI = (function () {
                 $(lThiz.header).addClass('top');
             }
 
-            window.addEventListener('scroll', function (event) {
-                var st = $(this).scrollTop();
+            window.addEventListener(
+                'scroll',
+                function (event) {
+                    var st = $(this).scrollTop();
 
-                if (pos < st) {
-                    $(lThiz.header).removeClass('top');
-                } else if (st === 0) {
-                    $(lThiz.header).addClass('top');
-                } else {
-                    $(lThiz.header).removeClass('top');
-                }
+                    if (pos < st) {
+                        $(lThiz.header).removeClass('top');
+                    } else if (st === 0) {
+                        $(lThiz.header).addClass('top');
+                    } else {
+                        $(lThiz.header).removeClass('top');
+                    }
 
-                pos = st;
-            }, false);
+                    pos = st;
+                },
+                false
+            );
 
             // sub page == gnb active
-            var pageTitle = document.querySelector("h6");
+            var pageTitle = document.querySelector('h6');
             var currentTitle = pageTitle.innerHTML;
 
-            $(".sub-menu .list-depth1 li a[name=link]").each(function() { 
-                var pageItem = $(this).attr("data-index"); 
+            $('.sub-menu .list-depth1 li a[name=link]').each(function () {
+                var pageItem = $(this).attr('data-index');
 
-                if(pageItem == currentTitle) {
-                    $(this).addClass("active")
-                } 
-                else {
-                // $(this).removeClass('active');
+                if (pageItem == currentTitle) {
+                    $(this).addClass('active');
+                } else {
+                    // $(this).removeClass('active');
                 }
             });
         },
@@ -52,15 +55,15 @@ var UI = (function () {
              *  }
              */
             var lLayer = $(pOption.selId);
-            var dim = $(pOption.selId).find(".dim");
+            var dim = $(pOption.selId).find('.dim');
 
-            if (pOption.st !== "close") {
+            if (pOption.st !== 'close') {
                 lLayer.addClass('active').fadeIn(300);
                 dim.show();
-                $("body").css({ overflow: "hidden" });
+                $('body').css({ overflow: 'hidden' });
 
-                if (pOption.selId == "#pop-recruit") {
-                    var recruitSwiper = new Swiper(".recruit-slider", {
+                if (pOption.selId == '#pop-recruit') {
+                    var recruitSwiper = new Swiper('.recruit-slider', {
                         loop: true,
                         slidesPerView: 1,
                         pagination: {
@@ -72,29 +75,37 @@ var UI = (function () {
                             formatFractionTotal: function (number) {
                                 return ('0' + number).slice(-2);
                             },
-                            renderFraction: function (currentClass, totalClass) {
-                                return '<em class="' + currentClass + '"></em>' +
+                            renderFraction: function (
+                                currentClass,
+                                totalClass
+                            ) {
+                                return (
+                                    '<em class="' +
+                                    currentClass +
+                                    '"></em>' +
                                     '<span> / </span>' +
-                                    '<em class="' + totalClass + '"></em>';
-                            }
+                                    '<em class="' +
+                                    totalClass +
+                                    '"></em>'
+                                );
+                            },
                         },
                         navigation: {
-                            nextEl: ".swiper-button-next",
-                            prevEl: ".swiper-button-prev",
+                            nextEl: '.button-next',
+                            prevEl: '.button-prev',
                         },
                     });
                 }
-        
             } else {
                 lLayer.removeClass('active').fadeOut(300);
                 dim.hide();
-                $("body").css({ overflow: "" });
+                $('body').css({ overflow: '' });
             }
-            lLayer.find(".dim, .pop-close").on("click", function (e) {
+            lLayer.find('.dim, .pop-close').on('click', function (e) {
                 e.preventDefault();
                 lLayer.removeClass('active').fadeOut(300);
                 dim.hide();
-                $("body").css({ overflow: "" });
+                $('body').css({ overflow: '' });
             });
         },
     };
