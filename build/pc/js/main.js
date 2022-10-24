4497bb35703c645f8f9af628926a4bcdaa532c6c
김민혜 <kmhkgh@naver.com>
2022-10-24 15:09:43 +0900

    git hositing

--- build/pc/js/main.js
+++ build/pc/js/main.js
@@ -1,41 +1,36 @@
 $(document).ready(function () {
 	//product
 	var prdPos = [];
-	var visualSwiper = new Swiper(".visual-swiper", {
+	var visualSwiper = new Swiper('.visual-swiper', {
 		loop: true,
-        loopAdditionalSlides: 100,
-        slidesPerView: 1.2,
-        spaceBetween: 100,
-        centeredSlides: true,
-        pagination: {
-            el: '.swiper-paging',
-            type: 'fraction',
-            formatFractionCurrent: function (number) {
-                return ('0' + number).slice(-2);
-            },
-            formatFractionTotal: function (number) {
-                return ('0' + number).slice(-2);
-            },
-            renderFraction: function (currentClass, totalClass) {
-                return '<em class="' + currentClass + '"></em>' +
-                    '<span> / </span>' +
-                    '<em class="' + totalClass + '"></em>';
-            }
-        },
-        navigation: {
-            nextEl: ".swiper-button-box .button-next",
-            prevEl: ".swiper-button-box .button-prev",
-        },
-	});
-
-    window.onload = () => {
-        document.querySelectorAll(".work-item").forEach((item) => {
-            item.style.gridRowEnd = `span ${item.clientHeight + 140}`;
-        });
-        const wrap = document.querySelector(".work-item-list");
-        wrap.style.display = "grid";
-        wrap.style.gridTemplateColumns = "repeat(2, 670px)";
-        wrap.style.gap = "0px 60px";
-        wrap.style.gridAutoRows = "1px";
-    }
+		loopAdditionalSlides: 100,
+		slidesPerView: 1.2,
+		spaceBetween: 100,
+		centeredSlides: true,
+		pagination: {
+			el: '.swiper-paging',
+			type: 'fraction',
+			formatFractionCurrent: function (number) {
+        		return ('0' + number).slice(-2);
+      		},
+			formatFractionTotal: function (number) {
+				return ('0' + number).slice(-2);
+			},
+			renderFraction: function (currentClass, totalClass) {
+				return (
+					'<em class="' +
+					currentClass +
+					'"></em>' +
+					'<span> / </span>' +
+					'<em class="' +
+					totalClass +
+					'"></em>'
+				);
+			},
+    	},
+		navigation: {
+			nextEl: '.swiper-button-box .button-next',
+			prevEl: '.swiper-button-box .button-prev',
+		},
+  });
 });
--- build/pc/js/ui.js
+++ build/pc/js/ui.js
@@ -1,79 +1,136 @@
 var UI = (function () {
-    return {
-        init: function () {
-            this.event();
+  return {
+    init: function () {
+      this.event();
+      this.workList();
+    },
+    element: {
+      header: '.header-wrap',
+    },
+    event: function () {
+      var lThiz = this.element;
+      var pos = $('html,body').scrollTop();
+
+      //layout
+      if (pos === 0) {
+        $(lThiz.header).addClass('top');
+      }
+
+      window.addEventListener(
+        'scroll',
+        function (event) {
+          var st = $(this).scrollTop();
+
+          if (pos < st) {
+            $(lThiz.header).removeClass('top');
+          } else if (st === 0) {
+            $(lThiz.header).addClass('top');
+          } else {
+            $(lThiz.header).removeClass('top');
+          }
+
+          pos = st;
         },
-        element: {
-            header: '.header-wrap',
-        },
-        event: function () {
-            var lThiz = this.element;
-            var pos = $('html,body').scrollTop();
+        false
+      );
 
-            //layout
-            if (pos === 0) {
-                $(lThiz.header).addClass('top');
-            }
+      // sub page == gnb active
+      var pageTitle = document.querySelector('h6');
+      var currentTitle = pageTitle.innerHTML;
 
-            window.addEventListener('scroll', function (event) {
-                var st = $(this).scrollTop();
+      $('.sub-menu .list-depth1 li a[name=link]').each(function () {
+        var pageItem = $(this).attr('data-index');
 
-                if (pos < st) {
-                    $(lThiz.header).removeClass('top');
-                } else if (st === 0) {
-                    $(lThiz.header).addClass('top');
-                } else {
-                    $(lThiz.header).removeClass('top');
-                }
+        if (pageItem == currentTitle) {
+          $(this).addClass('active');
+        } else {
+          // $(this).removeClass('active');
+        }
+      });
+    },
+    layerPopUp: function (pOption) {
+      /*   pOption
+       *  {
+       *  	 state : 'open'  OR  'close'
+       *  	 selId : Layer Selector
+       *  }
+       */
+      var lLayer = $(pOption.selId);
+      var dim = $(pOption.selId).find('.dim');
 
-                pos = st;
-            }, false);
+      if (pOption.st !== 'close') {
+        lLayer.addClass('active').fadeIn(300);
+        dim.show();
+        $('body').css({ overflow: 'hidden' });
 
-            // sub page == gnb active
-            var pageTitle = document.querySelector("h6");
-            var currentTitle = pageTitle.innerHTML;
-
-            $(".sub-menu .list-depth1 li a[name=link]").each(function() { 
-                var pageItem = $(this).attr("data-index"); 
-
-                if(pageItem == currentTitle) {
-                    $(this).addClass("active")
-                } 
-                else {
-                // $(this).removeClass('active');
-                }
-            });
-        },
-        layerPopUp: function (pOption) {
-            /*   pOption
-             *  {
-             *  	 state : 'open'  OR  'close'
-             *  	 selId : Layer Selector
-             *  }
-             */
-            var lLayer = $(pOption.selId);
-            var dim = $(pOption.selId).find(".dim");
-
-            if (pOption.st !== "close") {
-                lLayer.addClass('active').fadeIn(300);
-                dim.show();
-                $("body").css({ overflow: "hidden" });
-            } else {
-                lLayer.removeClass('active').fadeOut(300);
-                dim.hide();
-                $("body").css({ overflow: "" });
-            }
-
-            lLayer.find(".dim, .pop-close").on("click", function (e) {
-                e.preventDefault();
-                lLayer.removeClass('active').fadeOut(300);
-                dim.hide();
-                $("body").css({ overflow: "" });
-            });
-        },
-    };
+        if (pOption.selId == '#pop-recruit') {
+          var recruitSwiper = new Swiper('.recruit-slider', {
+            loop: true,
+            slidesPerView: 1,
+            pagination: {
+              el: '.swiper-paging',
+              type: 'fraction',
+              formatFractionCurrent: function (number) {
+                return ('0' + number).slice(-2);
+              },
+              formatFractionTotal: function (number) {
+                return ('0' + number).slice(-2);
+              },
+              renderFraction: function (currentClass, totalClass) {
+                return (
+                  '<em class="' +
+                  currentClass +
+                  '"></em>' +
+                  '<span> / </span>' +
+                  '<em class="' +
+                  totalClass +
+                  '"></em>'
+                );
+              },
+            },
+            navigation: {
+              nextEl: '.button-next',
+              prevEl: '.button-prev',
+            },
+          });
+        }
+      } else {
+        lLayer.removeClass('active').fadeOut(300);
+        dim.hide();
+        $('body').css({ overflow: '' });
+      }
+      lLayer.find('.dim, .pop-close').on('click', function (e) {
+        e.preventDefault();
+        lLayer.removeClass('active').fadeOut(300);
+        dim.hide();
+        $('body').css({ overflow: '' });
+      });
+    },
+    workList: function () {
+      if (matchMedia('screen and (min-width: 1640px)').matches) {
+        //   1640보다 크면
+        document.querySelectorAll('.work-item').forEach(item => {
+          item.style.gridRowEnd = `span ${item.clientHeight + 140}`;
+        });
+        const wrap = document.querySelector('.work-item-list');
+        wrap.style.display = 'grid';
+        wrap.style.gridTemplateColumns = 'repeat(2, 670px)';
+        wrap.style.gap = '0px 60px';
+        wrap.style.gridAutoRows = '1px';
+      } else {
+        document.querySelectorAll('.work-item').forEach(item => {
+          item.style.gridRowEnd = `span ${item.clientHeight + 70}`;
+        });
+        const wrap = document.querySelector('.work-item-list');
+        wrap.style.display = 'grid';
+        wrap.style.gridTemplateColumns = 'repeat(2, 570px)';
+        wrap.style.gap = '0px 60px';
+        wrap.style.gridAutoRows = '1px';
+      }
+    },
+  };
 })();
 
 $(document).ready(function () {
-    UI.init();
+  UI.init();
 });
