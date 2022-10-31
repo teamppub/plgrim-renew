a6c82cf565878f59d6fe2382520f17130578ed67
sallykwak <kwak0630@gmail.com>
2022-10-31 15:55:33 +0900

    pc main swiper -> realIndex

--- pc/js/main.js
+++ pc/js/main.js
@@ -10,44 +10,34 @@ $(document).ready(function () {
 		slidesPerView: "auto",
 		spaceBetween: 100,
 		centeredSlides: true,
-		pagination: {
-			el: '.swiper-paging',
-			type: 'fraction',
-			formatFractionCurrent: function (number) {
-        		return ('0' + number).slice(-2);
-      		},
-			formatFractionTotal: function (number) {
-				return ('0' + number).slice(-2);
-			},
-			renderFraction: function (currentClass, totalClass) {
-				return (
-					'<em class="' +
-					currentClass +
-					'"></em>' +
-					'<span> / </span>' +
-					'<em class="' +
-					totalClass +
-					'"></em>'
-				);
-			},
-    	},
 		navigation: {
 			nextEl: '.swiper-button-box .button-next',
 			prevEl: '.swiper-button-box .button-prev',
 		},
 		breakpoints: {
 			1640: {
-			  spaceBetween: 50,
+				spaceBetween: 50,
 			},
-		  },
+		},
+		on: {
+			slideChange: function() {
+				visualChange(this);
+			},
+		}
   	});
+	function visualChange(slide){
+		// console.log('slide가 바뀐 후 실행');
+		// console.log("0" + (slide.realIndex + 1));
+		var currentCount = "0" + (slide.realIndex + 1)
+		var totalCount = "0" + (slide.loopedSlides);
+		$(".swiper-paging .current-count").text(currentCount)
+		$(".swiper-paging .total-count").text(totalCount)
+	}
 
 	$('.visual-swiper .swiper-slide').on('mouseenter', function(e){
-		// console.log('stop autoplay');
 		visualSwiper.autoplay.stop();
 	})
 	$('.visual-swiper .swiper-slide').on('mouseleave', function(e){
-		// console.log('start autoplay');
 		visualSwiper.autoplay.start();
 	})
 });
