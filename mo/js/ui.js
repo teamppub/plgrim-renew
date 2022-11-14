8a0e583f8996fc85ed5bd38e8728a574aefea70a
jung won <59630012+kararico@users.noreply.github.com>
2022-11-14 13:50:20 +0900

    mobile  수정
    mobile  수정사항
    메인 카드리스트 json파일 생성 및 work페이지 더보기 작업완료,
    mobile gnb motion 추가 ( gnb open시 gsap  적용)

--- mo/js/ui.js
+++ mo/js/ui.js
@@ -1,13 +1,22 @@
+/* 페이징 처리 */
+var sOriginImgUrl =  window.location.pathname;
+var arSplitUrl   = sOriginImgUrl.split("/");    
+var nArLength     = arSplitUrl.length;
+var arFileName   = arSplitUrl[nArLength-1];   
+var arSplitFileName     = arFileName.split(".")[0];  
+
 var UI = (function () {
     return {
         init: function () {
             this.event();
+            this.workList();
         },
         element: {
             header: '.header-wrap',
             gnbBtn: '.btn-gnb',
             gnbWrap: '.gnb-wrap',
-            top: '#btn-top'
+            top: '#btn-top',
+            btnViewArea : '.btn-wrap'  
         },
         event: function () {
             var lThiz = this.element;
@@ -37,9 +46,22 @@ var UI = (function () {
 
             
 			$(lThiz.gnbBtn).on('click', function() {
-				$(this).find(".icon-menu").toggleClass("open");
-				$("body").toggleClass("fixed")
+				var navItem = $( ".gnb-list" ).find( "li" );
+                var itemDelay =  0.05;
+                var itemSpeed = 0.55;
+                if( !$(this).find(".icon-menu").hasClass( "open" )){
+                    navItem.each( function( index, item ){
+                        gsap.set( item, { x: (navItem.eq(index).width()) , opacity:0});
+                        gsap.to( item, itemSpeed,{ x:0, opacity:1,  ease:Sine.easeOut, delay: itemDelay * index })
+                    })
+                }else{
+                    gsap.killTweensOf(navItem);
+                }
+                
+                $(this).find(".icon-menu").toggleClass("open");
+                $("body").toggleClass("fixed");
 				$(lThiz.gnbWrap).toggleClass('active');
+
 				// gnbBtn.toggleClass("active");
 			});
 
@@ -121,11 +143,88 @@ var UI = (function () {
                 $("body").css({ overflow: "" });
             });
         },
+
+        workList: function(){
+            var dataURL = "../../html/json/work.json";
+            var lThiz = this.element;
+
+            $.ajax ({
+                type : 'get',
+                url: dataURL,
+                datatype : 'json',
+                success : function(data){   
+                  var max = 4;
+                  var start = 0;
+                  //처음 데이터가 없을경우 화살표 제거
+                  if( data.worksListItem.length == max ){
+                    $(lThiz.btnViewArea).css( "display", "none");
+                  }else{
+                    $(lThiz.btnViewArea).css( "display", "block");
+                  }
+                  //json data 
+                  for( let i = start; i < max; i++){
+                    UI.workView(data, i);
+                  }   
+        
+                  //버튼 더보기
+                  var btnView = '';
+                  btnView += '<div class="btn-wrap">',
+                  btnView += '<a href="javascript:;" class="btn-type">',
+                  btnView += '<i class="ico-arr down"><span class="hidden">더보기</span></i>',
+                  btnView += '</a>'
+                  btnView += '<p class="more">MORE</p>',
+                  btnView += '</div>'
+                  if( arSplitFileName == "main" ) return;
+                  $( ".work-item-list" ).append( btnView ) ;
+        
+                  var _cnt = 1;
+                  //더보기 클릭 시
+                  $( ".btn-type" ).on( "click", function(e){
+                    _cnt++;
+                    //더보기 클릭 이후 갯수 체크 이후 버튼 삭제
+                    if( data.worksListItem.length == data.worksListItem.length ){
+                      $(lThiz.btnViewArea).css( "display", "none");
+                    }
+                    //더보기 아이템 출력
+                    for( let i=max*(_cnt-1);i<  data.worksListItem.length; i++){
+                        UI.workView(data, i);
+                        if( arSplitFileName == "main" ){
+                            $( ".btn-wrap").remove();
+                            console.log( "메인?" );
+                        };
+                    }
+                  });
+                },
+                error : function(err){
+                  console.log('err : ',err)
+                }
+            });
+        },
+        workView: function(data, i){
+            var str = '';
+                str += '<div class="work-item">',
+                str += '<a href="javascript:;" data-id="'+data.worksListItem[i].id+'" data-link="'+data.worksListItem[i].itemLink+'" class="btn_work_link">',
+                str += '<div class="item-title">'+data.worksListItem[i].itemTitle+'</div>',
+                str += '<div class="item-img"><img src="'+data.worksListItem[i].itemImgSrc+'" alt="'+data.worksListItem[i].itemImgArt+'"></div>',
+                str += '<div class="item-sub">'+data.worksListItem[i].itemSubText+'</div>',
+                str += '</div>',
+                str += '</a>',
+                $( ".work-item-list" ).append( str );
+                if( arSplitFileName == "main" ){
+                    // $( ".btn-wrap").remove();
+                };
+                //아이템 클릭시 페이지 이동
+                $( ".btn_work_link" ).on( "click", function(){
+                    var link = $( this )[0].dataset.link;
+                    var itemId = $( this )[0].dataset.id;
+                    location.href = link;
+                    localStorage.setItem('pageId', itemId);
+                })
+        },
     };
 })();
 
 $(document).ready(function () {
     UI.init(); 
-    console.log( gsap );
 });
  
